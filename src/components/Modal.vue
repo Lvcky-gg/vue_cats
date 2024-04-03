3331f96f19b3343ecc69dfe1ff37976424b48054
lvcky-gg <matadorcrab@gmail.com>
2024-04-03 16:10:30 -0400

    modal is started

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,17 @@
-<script>
+<script setup>
+import { useModalStore } from '../stores/index'
+const modalStore = useModalStore();
+const changeModal =() =>{
+    modalStore.value = false;
+};
 </script>
 
 <template>
-    <h1>Hello World</h1>
+    <div class="fixed inset-x-1 inset-y-0 w-full h-full bg-transparant" @click="changeModal()">
+        <div class=" m-16 w-2/3 p24 bg-black">
+            <h1>Hello World</h1>
+        </div>
+        
+    </div>
+    
 </template>
