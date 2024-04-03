5839b22e98f92556ff751f2a127cbca160312aba
lvcky-gg <matadorcrab@gmail.com>
2024-04-03 16:34:24 -0400

    modal exists

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -11,7 +11,7 @@ const changeModal =() =>{
 
 <template>
     <div class="fixed inset-x-1 inset-y-0 w-full h-full bg-transparant" @click="changeModal()">
-        <div class=" m-16 w-2/3 p24 bg-black">
+        <div class=" m-auto mt-24 w-2/3 h-2/3 rounded-lg p24 bg-black">
             <h1>Hello World</h1>
         </div>
         
