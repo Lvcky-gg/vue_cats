512dca98dcfb7bbfa34e4b353e011425a6d149f0
lvcky-gg <matadorcrab@gmail.com>
2024-04-03 14:41:22 -0400

    started making the modal

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,30 @@
 import {  onMounted,  ref} from 'vue';
 import Card from 'primevue/card';
 import { useCatStore } from './stores/index'
-
+import Modal from './components/Modal.vue';
 
 const catStore = useCatStore();
 const loading = ref(true);
-
+let showModal = ref(false);
 onMounted(async() => {
     await catStore.fetchCats();
     loading.value = false;
 });
-console.log(catStore)
-
-
 </script>
 
-<template>
 
+
+<template>
   <div class="grid grid-cols-5">
   <Card class="w=1/2 rounded-lg bg-black m-2" v-for="cat in catStore.cats" :key="cat.id">
     <template #title>{{  }}</template>
     <template #content class="">
       <img class="p-6 m-auto " :src="cat.url" alt="cat" />
-      <button class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto mb-2">Learn More</button>
+      <button class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-auto mb-2" @click="showModal=true">Learn More</button>
     </template>
   </Card>
 </div>
+<Modal v-if="showModal" @close="showModal = false"></Modal>
 
 </template>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+    <h1>Hello World</h1>
+</template>
